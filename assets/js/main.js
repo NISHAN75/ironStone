b8c0d24cb524839a7581c26f7b7403e1665091e1
NISHAN75 <nishandas880@gmail.com>
2024-04-03 20:54:51 +0600

    all is ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,4 +69,62 @@
 
     });
  })(jQuery);
+
+
+
+
+
  
+
+
+
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  // Split text into spans
+  let typeSplit = new SplitType(".text-animation", {
+    types: "words, chars",
+    tagName: "span"
+  });
+
+  // Link timelines to scroll position
+  function createScrollTrigger(triggerElement, timeline) {
+    // Reset tl when scroll out of view past bottom of screen
+    ScrollTrigger.create({
+      trigger: triggerElement,
+      start: "top bottom",
+      onLeaveBack: () => {
+        timeline.progress(0);
+        timeline.pause();
+      }
+    });
+    // Play tl when scrolled into view (60% from top of screen)
+    ScrollTrigger.create({
+      trigger: triggerElement,
+      start: "top 70%",
+      onEnter: () => timeline.play()
+    });
+  }
+
+
+
+
+
+  $(".text-animation").each(function (index) {
+    let tl = gsap.timeline({ paused: true });
+    tl.from($(this).find(".char"), { yPercent: 100, duration: 0.2, ease: "power1.out", stagger: { amount: 0.6 } });
+    createScrollTrigger($(this), tl);
+  });
+
+ 
+
+  // Avoid flash of unstyled content
+  gsap.set(".text-animation", { opacity: 1 });
+});
+
+
+
+
+
+
+
+
